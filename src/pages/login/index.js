5cbbad8e62025b70aaa0332329f4825684c59cce
Ana Freitas <anaflaviafemme@gmail.com>
2021-02-10 14:04:57 -0300

    begin route

---
+++ src/pages/login/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Login = () => <h1>Login</h1>
+
+    // <div class='container'> 
+    //     <form action="Acesso">
+    //     <div class="form">
+    //     <input type="email" class='acessar' id='email' placeholder='Digite o seu email' autocomplete='off'>
+    // </div>
+    // <div class='form'>
+    //     <input type='password' class='acessar' id='senha' placeholder='Senha' autocomplete='off'>
+    // </div>
+    // </form>
+    //     <button class='btn'id='btnLogin'>
+    //     Iniciar sessão 
+    //     </button> 
+    //     <ul>     
+    //     <li id="btnRegister">Não possui conta?
+    //         <a href="/register">Cadastre-se</a>
+    //     </li>
+    //     </ul> 
+    // </div>
+    // `;
+
+export default Login;

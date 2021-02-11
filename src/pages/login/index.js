400106e29bbfb6f3c2d5904df8808dcd94ae5a1b
BiancaSherika <bianca_sherika@hotmail.com>
2021-02-10 23:46:16 -0300

    rotas entre login e register e inputs criados

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,25 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { history } from '../../history'
 
-const Login = () => <h1>Login</h1>
 
-    // <div class='container'> 
-    //     <form action="Acesso">
-    //     <div class="form">
-    //     <input type="email" class='acessar' id='email' placeholder='Digite o seu email' autocomplete='off'>
-    // </div>
-    // <div class='form'>
-    //     <input type='password' class='acessar' id='senha' placeholder='Senha' autocomplete='off'>
-    // </div>
-    // </form>
-    //     <button class='btn'id='btnLogin'>
-    //     Iniciar sessão 
-    //     </button> 
-    //     <ul>     
-    //     <li id="btnRegister">Não possui conta?
-    //         <a href="/register">Cadastre-se</a>
-    //     </li>
-    //     </ul> 
-    // </div>
-    // `;
+const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const routerRegister = () => {
+        history.push('/register')
+    }
+
+    const btnLogin = (event) => {
+        event.preventDefault();
+        console.log("fez login")
+    }
+
+    return (
+        <div className="container-login">
+            <h1 className="title-login">Login</h1>
+            <form className="form-login">
+                <input type="email" id="email-login" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} /><br />
+                <input type="password" id="pass-login" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)} /> <br />
+                <button className="button-login" onClick={btnLogin}>Login</button><br />
+                <p className="question-register">Primeiro dia aqui? <span className="button-back-register" onClick={routerRegister}>Crie sua conta aqui!</span></p>
+            </form>
+        </div>
+    );
+};
+
+
+// <div class='container'> 
+//     <form action="Acesso">
+//     <div class="form">
+//     <input type="email" class='acessar' id='email' placeholder='Digite o seu email' autocomplete='off'>
+// </div>
+// <div class='form'>
+//     <input type='password' class='acessar' id='senha' placeholder='Senha' autocomplete='off'>
+// </div>
+// </form>
+//     <button class='btn'id='btnLogin'>
+//     Iniciar sessão 
+//     </button> 
+//     <ul>     
+//     <li id="btnRegister">Não possui conta?
+//         <a href="/register">Cadastre-se</a>
+//     </li>
+//     </ul> 
+// </div>
+// `;
 
 export default Login;
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,5 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+// import { useHistory } from 'react-router-dom'
+import { history } from '../../history'
 
-const Register = () => <h1>Cadastro</h1>
+const Register = () => {
+
+    const [name, setName] = useState('');
+    const [option, setOption] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const routerLogin = () => {
+        history.push('/')
+    }
+
+    const btnRegister = (event) => {
+        event.preventDefault();
+        console.log("fez cadastro")
+    }
+
+    return (
+        <div className="container-register">
+            <h1 className="title-register">Cadastro</h1>
+            <form className="form-register">
+                <input id="name-register" placeholder="Nome" value={name} onChange={(e) => setName(e.target.value)} /><br />
+                <input type="radio" id="option-hall" name="option-register" value={option} onChange={(e) => setOption(e.target.value)} />
+                <label id="hall">Sou garçonete ou garçom</label><br />
+                <input type="radio" id="option-kitchen" name="option-register" value={option} onChange={(e) => setOption(e.target.value)} />
+                <label id="kitchen">Sou cozinheira ou cozinheiro</label><br />
+                <input type="email" id="email-register" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} /><br />
+                <input type="password" id="pass-register" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)} /> <br />
+                <button className="button-register" onClick={btnRegister}>Cadastrar</button><br />
+                <p className="question-login"> Já tem uma conta? <span className="button-back-login" onClick={routerLogin}>Faça Login aqui!</span></p>
+            </form>
+        </div>
+    );
+};
 
 export default Register;

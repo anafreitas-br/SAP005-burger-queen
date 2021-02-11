1161233f6068caf44394c8cc0cadbf89e88b9f5f
Ana Freitas <anaflaviafemme@gmail.com>
2021-02-11 18:00:09 -0300

    route-login/register

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,26 +1,19 @@
 
 import React from 'react'
-
+import {useHistory} from 'react-router-dom'
 import { ErrorMessage, Formik, Form, Field } from 'formik'
 import * as yup from 'yup'
 import axios from 'axios'
-import { history } from '../../history'
 import LogoVector from '../login/LogoVector.png'
 
 import './Login.css'
 
-const Login = () => {
-    const handleSubmit = values => {
-        axios.post('http://https://lab-api-bq.herokuapp.com/auth', values)
-            .then(resp => {
-                const { data } = resp
-                if (data) {
-                    localStorage.setItem('app-token', data)
-                    history.push('/')
-                }
-            })
-    }
-
+    const Login = () => {
+        
+    const history = useHistory();    
+    const handleSubmit = values => console.log(values)
+        
+    const routerRegister = () => { history.push('/register') } 
     const validations = yup.object().shape({
         email: yup.string().email().required(),
         password: yup.string().min(6).required()
@@ -30,35 +23,19 @@ const Login = () => {
             <img className="Logo" src = { LogoVector }/>
             <h1>Faça seu login</h1>
             <p>preencha os campos abaixo</p>
-            <Formik
-                initialValues={{}}
-                onSubmit={handleSubmit}
-                validationSchema={validations}
-            >
+            <Formik initialValues={{}} onSubmit={handleSubmit} validationSchema={validations}>
                 <Form className="Login">
                     <div className="Login-Group">
-                        <Field
-                            name="email"
-                            className="Login-Field"
-                        />
-                        <ErrorMessage
-                            component="span"
-                            name="email"
-                            className="Login-Error"
-                        />
+                        <Field name="email" className="Login-Field"/>
+                        <ErrorMessage component="span"name="email"className="Login-Error"/>
                     </div>
                     <div className="Login-Group">
-                        <Field
-                            name="password"
-                            className="Login-Field"
-                        />
-                        <ErrorMessage
-                            component="span"
-                            name="password"
-                            className="Login-Error"
-                        />
+                        <Field name="password"className="Login-Field"/>
+                        <ErrorMessage component="span"name="password"className="Login-Error"/>
                     </div>
                     <button className="Login-Btn" type="submit">Entrar</button>
+                    <p className="question-register"> Primeiro dia aqui? <span className="button-back-register" 
+                    onClick={routerRegister}>Cadastre-se !</span></p>                     
                 </Form>
             </Formik>
         </>

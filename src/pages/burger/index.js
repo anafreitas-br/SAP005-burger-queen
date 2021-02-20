0b37d598ae6fdd6b813c4a990b2c6b25053f03fc
anafreitas-br <anaflaviafemme@gmail.com>
2021-02-20 11:52:39 -0300

    menu all-day printado na tela e botões

--- src/pages/burger/index.js
+++ src/pages/burger/index.js
@@ -1,7 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 
-const Burger = () => <h1>Burgeres</h1>
+const Burger = () => {
+
+     const token = localStorage.getItem('token')
+     const [menuBurger, setMenuBurger] = useState('');
+  
+   
+
+    useEffect (() => {
+        fetch('https://lab-api-bq.herokuapp.com/products', {
+            method: 'GET',
+            headers: {
+            'Content-Type': 'application/json',
+            "Authorization": `${token}`
+          },
+        })
+        .then((response) => response.json())
+        .then((json) => {
+            console.log (json)
+          const burger = json.filter(item => item.type === 'all-day')
+        //   const burgerSlice = burger.slice([2,3])
+        //   let list = []
+        //   list = list.map(burgerSlice)
+          setMenuBurger(burger)
+        })
+      })
+
+
+      return (
+        <div className="Burger">
+          <div className="MenuBurger">
+
+            {menuBurger && menuBurger.map(function (item) {
+                    return (
+                        <div className="printScreen" key={item.name} name={item.name} id={item.id} price={item.price}>
+                            <p className="nameProduct">{item.name} {item.flavor} {item.complement} R$ {item.price},00
+                            <button className="btnAdd"> Adicionar</button></p>
+                        
+                        </div>
+                    );
+                })}
+            
+                            
+              <button className="btnFinal">Finalizar</button>
+             
+              
+           
+          </div>
+        </div>
+      );
+    }
+
 
 
 export default Burger;

1ad8e88512188136cccabe9f2ba788bde2b920d0
Bianca Sherika <bianca_sherika@hotmail.com>
2021-02-21 09:12:14 -0300

    mesa e nome do cliente permance na página e atualiza síncrono

--- src/pages/hall/index.js
+++ src/pages/hall/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import InnerHeader from '../../components/InnerHeader'
 import Client from '../../components/Client'
@@ -12,11 +12,9 @@ const Hall = () => {
     const token = localStorage.getItem("token");
     const id = localStorage.getItem("id");
 
-    const keepClient = (event) => {
-        event.preventDefault();
+    const keepClient = () => {
         localStorage.setItem("client", client);
-        localStorage.setItem("table", table)
-        Client();
+        localStorage.setItem("table", table);
     }
 
     fetch(`https://lab-api-bq.herokuapp.com/users/${id}`,{
@@ -27,27 +25,27 @@ const Hall = () => {
     .then((response) => response.json())
     .then((json) => {  
         setProfessional(json.name)
+        localStorage.setItem("name", json.name)
     }) 
 
     return (
         <>
-            <InnerHeader professional={professional}/>
-                <form className="Order">
-                    <div className="Group">
-                        <p className="Texts"> Cliente <span className='required'> *</span> </p>
-                        <input name="client" className="FieldOrder" placeholder="nome" value={client} onChange={(e) => setClient(e.target.value)}/>
-                    </div>
-                    <div className="Group">
-                        <p className="Texts"> Mesa  <span className='required'> *</span> </p>
-                        <input type="number" name="table" className="FieldOrder" placeholder="n°..." value={table} onChange={(e) => setTable(e.target.value)} />
-                    </div>
-                    <button className="Button" type="submit" onClick={keepClient}>Registrar</button>
-                    <p className="Texts"> <Link to='/breakfast' className="Button-back" >Café da manhã</Link></p>
-                    <p className="Texts"> <Link to='/burger'className="Button-back" >Lanches</Link></p>
-                </form>
+            <InnerHeader professional={professional} client={client} table={table}/>
+            <form className="Order">
+                <div className="Group">
+                    <p className="Texts"> Cliente <span className='required'> *</span> </p>
+                    <input name="client" className="FieldOrder" placeholder="nome" value={client} onChange={(e) => setClient(e.target.value)}/>
+                </div>
+                <div className="Group">
+                    <p className="Texts"> Mesa  <span className='required'> *</span> </p>
+                    <input type="number" name="table" className="FieldOrder" placeholder="n°..." value={table} onChange={(e) => setTable(e.target.value)} />
+                </div>
+                <button className="Button" type="submit" onClick={keepClient}>Registrar</button>
+                <p className="Texts"> <Link to='/breakfast' className="Button-back" >Café da manhã</Link></p>
+                <p className="Texts"> <Link to='/burger'className="Button-back" >Lanches</Link></p>
+            </form>
         </>
     )
 }
 
 export default Hall;
-
